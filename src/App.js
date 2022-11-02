e4de35045efb120c0dcb3f540759ee53805bc79a
Lennie Nurse <lnursej@gmail.com>
2022-11-02 04:04:31 -0400

    "update"

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
-import { useState } from "react";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
+import { useState } from "react"
 /*
   Components
 */
-import Nav from "./components/common/Nav";
-import Footer from "./components/common/Footer";
-import Home from "./components/home/Home";
-import StaffList from "./components/staff/StaffList";
-import PetsList from "./components/pets/PetsList";
+import Nav from "./components/common/Nav"
+import Footer from "./components/common/Footer"
+import Home from "./components/home/Home"
+import StaffList from "./components/staff/StaffList"
+import PetsList from "./components/pets/PetsList"
 
 /*
   Data
   ---------------
   Note: Normally this data would be pulled from an API. It is not necessary, however, for this application.
 */
-import { employeeData } from "./data/employees.js";
-import { ownerData } from "./data/owners";
-import { petData } from "./data/pets";
+import { employeeData } from "./data/employees.js"
+import { ownerData } from "./data/owners"
+import { petData } from "./data/pets"
 
 function App() {
-  const [employees] = useState(employeeData);
-  const [owners] = useState(ownerData);
-  const [pets] = useState(petData);
+  const [employees] = useState(employeeData)
+  const [owners] = useState(ownerData)
+  const [pets] = useState(petData)
 
   return (
     <div className="wrapper">
-      <Nav />
-      <Home employees={employees} owners={owners} pets={pets} />
-      <StaffList employees={employees} />
-      <PetsList pets={pets} />
-      <Footer />
+      <Router>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Home employees={employees} owners={owners} pets={pets} />
+          <StaffList employees={employees} />
+          <PetsList pets={pets} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
